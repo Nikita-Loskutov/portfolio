11abc7d885e5ec20617ebfc698fd621783ee8448
Nikita-Loskutov <nikitaloskutiv@gmail.com>
2025-03-01 01:04:19 +0200

    update

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-first_block= document.getElementById("first_block")
-main_block= document.getElementById("main_block")
-body = document.getElementById("body")
-
+first_block = document.getElementById("first_block");
+main_block = document.getElementById("main_block");
+fade_block = document.getElementById("fade_block");
+body = document.getElementById("body");
 
 setTimeout(function() {
-    first_block.style.display = "none";
-    main_block.style.opacity = "100%";
-    body.style.align_items = "none";
-
-
+    fade_block.style.opacity = "1";
+    fade_block.style.filter = "blur(100px)";
+    setTimeout(function() {
+        first_block.style.display = "none";
+        main_block.style.opacity = "100%";
+        fade_block.style.opacity = "0";
+        fade_block.style.filter = "blur(0px)";
+    }, 1500);
 }, 2500);
