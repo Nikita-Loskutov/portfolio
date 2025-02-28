d0b478ec091c2ec5581d0af02be7f9dd8ea943bb
Nikita-Loskutov <nikitaloskutiv@gmail.com>
2025-02-28 23:22:05 +0200

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+first_block= document.getElementById("first_block")
+main_block= document.getElementById("main_block")
+body = document.getElementById("body")
+
+
+setTimeout(function() {
+    first_block.style.display = "none";
+    main_block.style.opacity = "100%";
+    body.style.align_items = "none";
+
+
+}, 2500);
